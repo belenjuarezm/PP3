1822c4c7fac7034f33d0fb94b3ea7016f5ea2ea9
belenjuarezm <beljrz@hotmail.com>
2024-06-19 11:13:28 -0300

    ultimos cambios

--- Tpcarrito/Program.cs
+++ Tpcarrito/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Tpcarrito.models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<tpcarritoContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
+
 
 var app = builder.Build();
 
